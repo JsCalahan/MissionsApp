5bea7057c17e6bc8a0e6445343c7cf7f535c6f30
0770724 <0770724@KELCE-ACIS10-P>
2017-10-23 18:08:32 -0500

    Mission/Event Creation

--- MissionsApp1/MissionsApp1/Pages/NewEvent.xaml.cs
+++ MissionsApp1/MissionsApp1/Pages/NewEvent.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MissionsApp1.Classes;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -19,6 +20,18 @@ namespace MissionsApp1.Pages
 
         private void CreateEvent_Clicked(object sender, EventArgs e)
         {
+            Missions Mission = new Missions();
+            Mission.Name = EventName.Text;
+            Mission.Address = EventAddress.Text;
+            //Mission.City = EventCity.Text;
+            //Mission.State = EventState.Text;
+            //Mission.ZipCode = EventZipCode.Text;
+            //Mission.Latitude = EventLatitude.Text;
+            //Mission.Longitude = EventLongitude.Text;
+            Mission.Date = EventDate.Text;
+            Mission.StartTime = EventStartTime.Text;
+            Mission.EndTime = EventEndTime.Text;
+
             Navigation.PushAsync(new EventCreated());
         }
     }
