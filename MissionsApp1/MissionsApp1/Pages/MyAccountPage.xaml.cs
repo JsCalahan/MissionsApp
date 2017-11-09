8fc24cbc1964347fbe1320ca5f3aa92c6e0facd7
Brett <0705768@KELCE-ACIS09-P>
2017-11-08 18:10:03 -0600

    Took out Event created page/visual edits

--- MissionsApp1/MissionsApp1/Pages/MyAccountPage.xaml.cs
+++ MissionsApp1/MissionsApp1/Pages/MyAccountPage.xaml.cs
@@ -11,9 +11,9 @@ using Xamarin.Forms.Xaml;
 
 namespace MissionsApp1.Pages
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class MyAccountPage : ContentPage
-	{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MyAccountPage : ContentPage
+    {
 
         public ObservableCollection<string> Missions; // { get; set; }
 
@@ -25,26 +25,14 @@ namespace MissionsApp1.Pages
             /*this.Missions = new ObservableCollection<Mission>(databaseMission);
 
             this.UserEventListView.ItemsSource = this.Missions;
-        */}
-        public MyAccountPage ()
-		{
-			InitializeComponent ();
+        */
+        }
+        public MyAccountPage()
+        {
+            InitializeComponent();
 
             this.Missions = new ObservableCollection<string>();
         }
 
-        private void AddMissionButton_Clicked(object sender, EventArgs e)
-        {
-            if (String.IsNullOrEmpty(NewMissionEntry.Text))
-            {
-                DisplayAlert("Error", "Entry is empty.", "Ok");
-            }
-            else
-            {
-                this.Missions.Add(NewMissionEntry.Text);
-                UserEventListView.ItemsSource = this.Missions;
-                NewMissionEntry.Text = "";
-            }
-        }
     }
 }
